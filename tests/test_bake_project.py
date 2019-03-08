2fcd66abd619d995cfb6861bf54a00f783dde3e5
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-03-08 06:24:11 -0800

    Fixed messy dict definition.

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -24,7 +24,8 @@ def test_project_tree(cookies):
     result = cookies.bake(extra_context={
         "package_name": "test_project",
         "package_slug": "test-project",
-        "package_title": "Test Project"
+        "package_title": "Test Project",
+        "package_short_description": "Short description."
     })
     assert result.exit_code == 0
     assert result.exception is None
